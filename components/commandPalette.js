2535eead9f4cbfc7bf8d9de7023f345b7e6bfede
tejasc0 <tejasc888@gmail.com>
2022-03-25 09:40:10 +0000

    removed headless ui

--- components/commandPalette.js
+++ components/commandPalette.js
@@ -1,29 +1,8 @@
-import { Dialog, Combobox } from '@headlessui/react'
 import { useState } from 'react'
-import { SearchIcon } from '@heroicons/react/outline'
+
 const CommandPalette = ({ countries }) => {
-  console.log(countries)
-  const [isOpen, setIsOpen] = useState(true)
   return (
-    <Dialog open={isOpen} onClose={setIsOpen}
-      className="fixed inset-0 overflow-y-auto p-4 pt-[25vh]"
-    >
-      <Dialog.Overlay className="fixed inset-0 bg-gray-500/75" />
-      <Combobox as="div" className="relative mx-auto max-w-xl rounded-md bg-white shadow-2xl">
-        <div className="flex items-center">
-          <Combobox.Input className="w-full bg-transparent text-sm overflow-hidden text-gray-800 placeholder-gray-400 h-12 p-3" placeholder="Search..." />
-        </div>
-        <Combobox.Options static className="py-4 text-sm max-h-96 overflow-y-auto">
-          {countries.countries.map((item) => {
-            return <div key={item.country}>
-              <Combobox.Option className="px-4 py-2 focus:bg-orange-400">{item.country}</Combobox.Option>
-            </div>;
-          })}
-
-        </Combobox.Options>
-      </Combobox>
-
-    </Dialog>
+    <div>jelkj</div>
   )
 }
 
